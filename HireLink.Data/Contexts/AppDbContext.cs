4d52b7d81f4ab941c4fceb32e5c13faf9b27a0ef
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-05 17:16:35 +0500

    Added DbContext and configured for SqlServer

---
+++ HireLink.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using HireLink.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireLink.Data.Contexts;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Candidate> Candidates { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var candidateBuilder = modelBuilder.Entity<Candidate>();
+        modelBuilder.Entity<Candidate>()
+            .HasIndex(c => c.Email)
+                .IsUnique();
+
+        #region Apply Case Insensitive Collation If Db is PostgreSQL
+        /*
+        if (Database.ProviderName == "Npgsql.EntityFrameworkCore.PostgreSQL")
+        {
+            modelBuilder
+                .HasCollation("case_insensitive_collation", locale: "en-u-ks-primary", provider: "icu", deterministic: false);
+
+            modelBuilder.Entity<Candidate>()
+                .Property(c => c.Email)
+                .UseCollation("case_insensitive_collation");
+        }
+        */
+        #endregion
+    }
+}
---
+++ HireLink.Data/Migrations/20240705121259_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HireLink.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TimeToCall = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinkedinUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GithubUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_Email",
+                table: "Candidates",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
