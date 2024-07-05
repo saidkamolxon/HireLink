a6538692224f47a747bbc0caf3d13269e470a809
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-05 17:16:49 +0500

    Added Repository

---
+++ HireLink.Data/IRepositories/ICandidateRepository.cs
@@ -0,0 +1,11 @@
+﻿using HireLink.Domain.Entities;
+
+namespace HireLink.Data.IRepositories;
+
+public interface ICandidateRepository
+{
+    Task InsertAsync(Candidate candidate, CancellationToken cancellationToken = default);
+    void Update(Candidate candidate);
+    Task<Candidate?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task SaveAsync(CancellationToken cancellationToken = default);
+}
---
+++ HireLink.Data/Repositories/CandidateRepository.cs
@@ -0,0 +1,26 @@
+﻿using HireLink.Data.Contexts;
+using HireLink.Data.IRepositories;
+using HireLink.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireLink.Data.Repositories;
+
+public class CandidateRepository(AppDbContext context) : ICandidateRepository
+{
+    private readonly AppDbContext context = context;
+
+    public async Task<Candidate?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        => await this.context.Candidates.FirstOrDefaultAsync(c => c.Email == email, cancellationToken);
+
+    public async Task InsertAsync(Candidate candidate, CancellationToken cancellationToken = default)
+        => await this.context.Candidates.AddAsync(candidate, cancellationToken);
+
+    public async Task SaveAsync(CancellationToken cancellationToken = default)
+        => await this.context.SaveChangesAsync(cancellationToken);
+
+    public void Update(Candidate candidate)
+    {
+        candidate.UpdatedAt = DateTime.UtcNow;
+        this.context.Entry(candidate).State = EntityState.Modified;
+    }
+}
