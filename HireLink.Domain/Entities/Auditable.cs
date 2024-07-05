4a72b2863018c8280aaa3ff5f2ce0648c3860ce7
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-05 17:14:50 +0500

    Created 'Candidate' entity

---
+++ HireLink.Domain/Entities/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace HireLink.Domain.Entities;
+
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ HireLink.Domain/Entities/Candidate.cs
@@ -0,0 +1,13 @@
+﻿namespace HireLink.Domain.Entities;
+
+public class Candidate : Auditable
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public string? TimeToCall { get; set; }
+    public string? LinkedinUrl { get; set; }
+    public string? GithubUrl { get; set; }
+    public required string Comment { get; set; }
+}
