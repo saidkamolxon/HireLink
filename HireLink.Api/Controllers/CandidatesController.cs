639ac007477dc7eecb398fef44262a20202bf3f9
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-06 17:31:02 +0500

    Created main controller (CandidatesController)

---
+++ HireLink.Api/Controllers/CandidatesController.cs
@@ -0,0 +1,14 @@
+﻿using HireLink.Service.DTOs;
+using HireLink.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireLink.Api.Controllers;
+
+public class CandidatesController(ICandidateService service) : BaseController
+{
+    private readonly ICandidateService service = service;
+
+    [HttpPost]
+    public async Task<ActionResult<CandidateResultDto>> UpsertAsync(CandidateUpsertDto dto)
+        => await this.service.UpsertAsync(dto);
+}
