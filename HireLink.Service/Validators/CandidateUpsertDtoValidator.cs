ad452fbf9fc27ffaf12b55666c098533d1ffb7c4
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-06 17:27:57 +0500

    Created Fluent Validator for CandidateUpsertDto

---
+++ HireLink.Service/Validators/CandidateUpsertDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using HireLink.Service.DTOs;
+
+namespace HireLink.Service.Validators;
+
+public class CandidateUpsertDtoValidator : AbstractValidator<CandidateUpsertDto>
+{
+    public CandidateUpsertDtoValidator()
+    {
+        RuleFor(c => c.FirstName)
+            .NotEmpty();
+
+        RuleFor(c => c.LastName)
+            .NotEmpty();
+
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(c => c.Comment)
+            .NotEmpty();
+    }
+}
