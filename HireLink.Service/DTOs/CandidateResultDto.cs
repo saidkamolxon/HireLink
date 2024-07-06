c87b9ab5f560aa5e3229d4db1a7cf86d24b7b83c
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-06 17:18:38 +0500

    Created DTOs as record

---
+++ HireLink.Service/DTOs/CandidateResultDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HireLink.Service.DTOs;
+
+public record CandidateResultDto(
+    long Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Comment,
+    string? PhoneNumber = default,
+    string? TimeToCall = default,
+    string? LinkedinUrl = default,
+    string? GithubUrl = default
+);
---
+++ HireLink.Service/DTOs/CandidateUpsertDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HireLink.Service.DTOs;
+
+public record CandidateUpsertDto(
+    string FirstName,
+    string LastName,
+    string Email,
+    string Comment,
+    string? PhoneNumber = default,
+    string? TimeToCall = default,
+    string? LinkedinUrl = default,
+    string? GithubUrl = default
+);
