5ee5d5f7b45776fc2ce8ad21bb1567b16a237399
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-07-06 17:28:11 +0500

    Created MappingProfile for AutoMapper

---
+++ HireLink.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HireLink.Domain.Entities;
+using HireLink.Service.DTOs;
+
+namespace HireLink.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CandidateUpsertDto, Candidate>();
+        CreateMap<Candidate, CandidateResultDto>();
+    }
+}
